00027daff0446c894a9084027af7fcef497db8b3
LucaElian <lucaelian42@gmail.com>
2026-08-15 18:55:35 -0300

    Add getters/setters and toString to Persona
    Implement getters and setters for all Persona fields (dni, nombre, apellido, edad, fechaNacimiento, genero, direccion, telefono, email), add a toString() override and small constructor/comment formatting. Also update .gitignore to ignore /.metadata/ and ensure newline at EOF.

--- TP1/TP1_v1/src/ejercicio1/Persona.java
+++ TP1/TP1_v1/src/ejercicio1/Persona.java
@@ -13,6 +13,9 @@ public class Persona {
     private String telefono;
     private String email;
     
+    
+    // constructor
+    
     public Persona() {
         this.nombre = "sin nombre";
         this.edad = 0;
@@ -24,5 +27,95 @@ public class Persona {
         this.telefono = "";
         this.email = "";
     }
+
     
+    // getters y setters
+    
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	// metodo toString()
+	
+	@Override
+	public String toString() {
+		return "DNI=" + dni + 
+				"\nnombre=" + nombre + 
+				"\napellido=" + apellido + 
+				"\nedad=" + edad +
+				"\nfechaNacimiento=" + fechaNacimiento + 
+				"\ngenero=" + genero + 
+				"\ndireccion=" + direccion +
+				"\ntelefono=" + telefono + 
+				"\nemail=" + email;
+	}
 }
