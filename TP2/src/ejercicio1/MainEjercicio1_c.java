2cc659a214b1581e3cdb7dfc6b0118f1011510bf
amilotich <alemilotich.94@gmail.com>
2026-09-01 07:46:51 -0300

    Crea y guarda empleados en TreeSet

---
+++ TP2/src/ejercicio1/MainEjercicio1_c.java
@@ -0,0 +1,24 @@
+package ejercicio1;
+
+import java.util.TreeSet;
+
+public class MainEjercicio1_c {
+
+	public static void main(String[] args) {
+		Empleado e1 = new Empleado();
+		Empleado e2 = new Empleado();
+		Empleado e3 = new Empleado();
+		Empleado e4 = new Empleado();
+		Empleado e5 = new Empleado();
+		
+		TreeSet<Empleado> listaEmpleados = new TreeSet<Empleado>();
+		
+		listaEmpleados.add(e1);
+		listaEmpleados.add(e2);
+		listaEmpleados.add(e3);
+		listaEmpleados.add(e4);
+		listaEmpleados.add(e5);
+
+	}
+
+}
