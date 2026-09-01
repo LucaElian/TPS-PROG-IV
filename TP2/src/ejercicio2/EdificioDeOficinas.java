db9d06aea8b2801170dd6a8167fbf2980653db48
laia <franco.laia18@gmail.com>
2026-09-01 17:33:01 -0300

    fix: agregar encapsulamiento y toString

--- TP2/src/ejercicio2/EdificioDeOficinas.java
+++ TP2/src/ejercicio2/EdificioDeOficinas.java
@@ -1,10 +1,11 @@
 package ejercicio2;
 
 public class EdificioDeOficinas extends Edificio {
+	
+	//atributos
 	private int cantidadOficinas;
 	
 	// constructores
-	
 	public EdificioDeOficinas() {
 		super();
 		this.cantidadOficinas = 0;
@@ -13,5 +14,21 @@ public class EdificioDeOficinas extends Edificio {
 	public EdificioDeOficinas(double superficie, int cantidadOficinas) {
 		super(superficie);
 		this.cantidadOficinas = cantidadOficinas;
+	}
+	
+	//metodos
+	public int getCantidadOficinas() {
+		return cantidadOficinas;
+	}
+	
+	public void setCantidadOficinas(int cantidadOficinas) {
+		this.cantidadOficinas = cantidadOficinas;
+	}
+
+	//metodo toString 
+	@Override
+	public String toString() {
+		return super.toString() + 
+		"\nCantidad de oficinas: " + cantidadOficinas;
 	}
 }
