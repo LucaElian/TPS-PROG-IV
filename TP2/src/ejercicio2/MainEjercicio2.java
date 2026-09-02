bec985d4604974caea79dc76e231a25e9161ce98
Mauro Sebastian Arias <maurosebastianarias@gmail.com>
2026-09-02 00:45:24 -0300

    Se agrego 2 objetos del tipo EdificioDeOficinas al ArrayList

--- TP2/src/ejercicio2/MainEjercicio2.java
+++ TP2/src/ejercicio2/MainEjercicio2.java
@@ -6,15 +6,20 @@ public class MainEjercicio2 {
 	
 	public static void main(String[] args) {
 		
+		ArrayList<Edificio> listaEdificios = new ArrayList<Edificio>();
+		
 		Polideportivo p1 = new Polideportivo(300.5, "Lionel Messi", 1);
 		Polideportivo p2 = new Polideportivo(103.8, "River", 2);
 		Polideportivo p3 = new Polideportivo(650, "Bombonera", 2);
+		EdificioDeOficinas edO1 = new EdificioDeOficinas();
+		EdificioDeOficinas edO2 = new EdificioDeOficinas(254.6, 15);
 		
-		ArrayList<Edificio> listaEdificios = new ArrayList<Edificio>();
 		
 		listaEdificios.add(p1);
 		listaEdificios.add(p2);
 		listaEdificios.add(p3);
+		listaEdificios.add(edO1);
+		listaEdificios.add(edO2);
 		
 		
 	}
