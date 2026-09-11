7815b079f253455e841000594bf711162e552b81
JonatanRosassss <jonatanisaiasrosas@gmail.com>
2026-09-10 23:00:30 -0300

    Se hizo la clase Producto con sus constructores y metodo ToString

---
+++ TP3/src/entidad/Producto.java
@@ -0,0 +1,88 @@
+package entidad;
+
+public class Producto {
+
+	private String codigo;
+	private String nombre;
+	private double precio;
+	private int stock;
+	private Categoria categoria;
+
+	public Producto() {
+		this.codigo = "";
+		this.nombre = "";
+		this.precio = 0;
+		this.stock = 0;
+		this.categoria = new Categoria();
+	}
+
+	public Producto(String codigo, String nombre, double precio, int stock, Categoria categoria) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+		this.categoria = categoria;
+	}
+
+	public Producto(String codigo, String nombre, double precio, int stock, int idCategoria) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+		this.categoria = new Categoria(idCategoria, "");
+	}
+
+	
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	// Metodo toString
+	@Override
+	public String toString() {
+		return "Producto: " + codigo +
+				", Nombre: " + nombre + 
+				", Precio: " + precio + 
+				", Stock: " + stock
+				+ ", " + 
+				(categoria != null ? categoria.toString() : "Sin categoria");
+	}
+
+}
